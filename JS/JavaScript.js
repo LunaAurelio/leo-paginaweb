aad475edc80979fc40342e672b99c4de294bb54b
lunaa <lunaaurelio03@gmail.com>
2023-04-12 20:57:40 -0300

    primer commit LS

---
+++ JS/JavaScript.js
@@ -0,0 +1,12 @@
+// const miDiv = document.getElementById('lector1');
+
+// miDiv.addEventListener('mouseover', () => {
+//   miDiv.style.width = '300px';
+//   miDiv.style.height = '150px';
+// });
+
+// miDiv.addEventListener('mouseout', () => {
+//   miDiv.style.width = '200px';
+//   miDiv.style.height = '100px';
+// });
+
